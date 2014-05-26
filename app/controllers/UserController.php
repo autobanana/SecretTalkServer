33fbcea96d0d4d0a0a4b2374f26161eb8e876840
CFWei <s124210@gmail.com>
2014-05-27 01:36:45 +0800

    add new controller

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,88 +1,34 @@
 <?php
 
-class UserController extends \BaseController {
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
+class UserController extends BaseController {
+		
+		
+	public function postIndex
 	{
-		//
+		return "123";
+	}
+
+	public function postCreate
+	{
+		
+	}
+
+	public function postLogin
+	{
+	
+	}
+
+	public function postLogout
+	{
+	
+	
 	}
 
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
 
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
 
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param int $id for show id
-	 *
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param int  $id 
-	 *
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+	
 
 
 }
